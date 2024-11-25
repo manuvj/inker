08cb5c44cd578e4773f5728f86bd0dbb97c9129f
Alan Francis <alan.prime567@gmail.com>
2024-11-25 17:15:13 +0530

    feat:add grid content

--- app/(root)/sections/pillars.tsx
+++ app/(root)/sections/pillars.tsx
@@ -6,24 +6,28 @@ const PillarSection = () => {
   const features = [
     {
       name: "ROBOLAND",
+      des: "An interactive tech hub for all ages, combining education and entertainment to make advanced technology engaging and accessible. Featuring hands-on exhibits, Tech demos, and Meta world experiences, it sparks curiosity and inspires everyone from beginners to tech enthusiasts, accessing grassroots innovation.",
       background: "/grid/grid1.webp",
       className:
         "aspect-square md:aspect-auto md:row-span-6 md:col-span-2 md:col-start-1 md:row-start-1 md:h-[600px]",
     },
     {
       name: "Techknowledge",
+      des: "Empowering freshers, professionals, and entrepreneurs with future-focused skills, Techknowledge Academy blends cutting-edge technology and hands-on learning. This hub is your gateway to upskilling, reskilling, and thriving in a tech-driven world. Share knowledge. Build futures. Innovate here.",
       background: "/grid/grid2.webp",
       className:
         "aspect-square md:aspect-auto md:col-span-4 md:row-span-4 md:h-[360px]",
     },
     {
       name: "Futuristech",
+      des: "Dedicated research and development center, where ideas are transformed into practical solutions. This is where technology meets purpose, aiming to address realworld challenges through collaboration and innovation.",
       background: "/grid/grid3.webp",
       className:
         "aspect-square md:aspect-auto md:col-start-3 md:row-start-1 md:col-span-2 md:row-span-3 md:h-[300px]",
     },
     {
       name: "Inkubator",
+      des: "Designed to help entrepreneurs and innovators bring their ideas to market. With structured incubation programs, expert mentorship, and access to advanced prototyping labs, it offers a comprehensive support system. The state-of-the-art infrastructure and technology make it a cost-effective solution for startups, reducing capital requirements and providing the best incubator experience.",
       background: "/grid/grid4.webp",
       className:
         "aspect-square md:aspect-auto md:col-start-5 md:row-start-1 md:col-span-2 md:row-span-3 md:h-[300px]",
@@ -42,7 +46,7 @@ const PillarSection = () => {
           <div
             key={index}
             className={cn(
-              "overflow-hidden rounded-lg relative filter saturate-50 hover:saturate-100 transition-all duration-300",
+              "group overflow-hidden rounded-lg relative filter saturate-50 hover:saturate-100 transition-all duration-300",
               feature.className
             )}
           >
@@ -53,8 +57,15 @@ const PillarSection = () => {
               className="object-cover object-center"
               sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
             />
-            <div className="p-4 font-orbitron text-white text-3xl font-bold absolute top-3 left-3 underline">
-              {feature.name}
+            <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-50 transition-opacity duration-500"></div>
+
+            <div className="absolute top-3 left-3 flex flex-col z-50">
+              <h1 className="p-4 font-orbitron text-white text-3xl font-bold underline">
+                {feature.name}
+              </h1>
+              <p className="p-4 text-white md:text-xl opacity-0 group-hover:opacity-100 transition-opacity duration-500">
+                {feature.des}
+              </p>
             </div>
           </div>
         ))}
